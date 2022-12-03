c87487f2f09dbbdd7a035c9405ebc4ba7be2a69f
Ian Waddington <iwaddo@icloud.com>
2022-12-03 11:37:38 +0000

    Initial Commit

---
+++ D33 Animations/D33_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  D33_AnimationsApp.swift
+//  D33 Animations
+//
+//  Created by Ian Waddington on 03/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct D33_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
